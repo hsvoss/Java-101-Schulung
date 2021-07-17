db4f0f18bd4d0ebb1a5909f8ca97b6586cb68404
Hendrik Voß <hendrik@familievoss.org>
2021-07-17 17:08:04 +0200

    Grundgerüst für Aufgaben der Session2

---
+++ Session 02 - java.util.Collection/src/main/java/AufgabenSession02.java
@@ -0,0 +1,11 @@
+import java.time.*;
+
+public class AufgabenSession02 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ Session 02 - java.util.Collection/src/test/java/AufgabenSession02Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class AufgabenSession02Test {
+
+
+}
