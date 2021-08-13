0550b441c522c167cc638f5062403edc900a19d6
Hendrik Voß <hendrik@familievoss.org>
2021-08-13 19:33:16 +0200

    MavenProjekt für Session04 angelegt

---
+++ Session 04 - Stream_Optional/src/main/java/AufgabenSession04.java
@@ -0,0 +1,14 @@
+public class AufgabenSession04 {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        task1();
+
+    }
+
+
+    static void task1() {
+        System.out.println("Hallo Welt");
+    }
+
+}
---
+++ Session 04 - Stream_Optional/src/test/java/AufgabenSession04Test.java
@@ -0,0 +1,8 @@
+class AufgabenSession04Test {
+
+    void task1() {
+
+    }
+
+
+}
