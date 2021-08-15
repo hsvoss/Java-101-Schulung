68870c218f3157e4c554064c59508d821aeecdac
Hendrik Voß <hendrik@familievoss.org>
2021-08-15 17:35:18 +0200

    Aufgaben für Session4

--- Session 04 - Stream_Optional/src/main/java/AufgabenSession04.java
+++ Session 04 - Stream_Optional/src/main/java/AufgabenSession04.java
@@ -1,7 +1,150 @@
+import model.Berater;
+import model.Branche;
+import model.Kunde;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class AufgabenSession04 {
 
-    public static void main(String[] args) {
+//    Stöbert in:
+//    https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/stream/package-summary.html
+//    https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/stream/Stream.html
+//    https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Optional.html
 
+
+    public static void main(String[] args) {
+        List<Berater> beraterList = Helper.generateData();
+        task6(beraterList);
+
+//        checkPrime(323); // false
+//        checkPrime(331); // true
     }
+
+    /**
+     * Schaue dir die Klassen im Package model an, damit du ein Gefühl für die Datenstruktur bekommst.
+     * Getter, Setter, equal, hashCode & toString enthalten nur generierten Code.
+     * <p>
+     * Die Methode Helper.generateData() generiert dir Testdaten. Wenn du die Daten ändern/erweitern möchtest,
+     * kopiere dir bitte diese Methode und manipuliere die Daten in der Kopie. Sonst funktionieren vielleicht die
+     * Folgeaufgaben nicht mehr.
+     */
+    static void task1() {
+        List<Berater> beraterList = Helper.generateData();
+    }
+
+
+    /**
+     * Gib alle Berater zurück, die nur bei einem Kunden im Einsatz sind.
+     *
+     * @return die gefilterte Liste
+     */
+    static List<Berater> task2(List<Berater> beraterList) {
+        return null;
+    }
+
+// Task 2b)
+//     * Schaue dir den Stream im Debugger an:
+//     * 1. Nutze die Stream-Trace-Funktion deiner IDE (https://www.jetbrains.com/help/idea/analyze-java-stream-operations.html)
+//     * 2. Setze einen Breakpoint nur an deinem Lambda-Ausdruck und nicht am '.filter'
+//     * 3. Setze einen Breakpoint am '.filter' und nicht am Lambda-Ausdruck.
+//     * 4. Setze einen Breakpoint sowohl am '.filter' als auch am Lambda-Ausdruck.
+
+
+    /**
+     * Gib die Namen der Berater zurück, die nur bei einem Kunden im Einsatz sind.
+     * Stelle sicher, dass kein Name doppelt vorkommt.
+     *
+     * @return die gefilterte Liste
+     */
+    static Collection<String> task3(List<Berater> beraterList) {
+
+        return null;
+    }
+
+
+    /**
+     * Gib alle Kunden-Namen zurück, bei denen mindestens 2 Berater im Einsatz sind.
+     * Stelle sicher, dass kein Kunde doppelt vorkommt.
+     * Sortiere die Kunden-Firmennamen alphabetisch.
+     * <p>
+     * Wenn du ein Problem mit dem Return-Type Set<String> hast, dann ändere ihn auf Collection<String>
+     *
+     * @return die gefilterte Collection
+     */
+    static Set<String> task4(List<Berater> beraterList) { // schwer
+//    static Collection<String>  task4(List<Berater> beraterList) { // einfach
+
+        return null;
+    }
+
+    /**
+     * Liste alle Kunden anhand ihrer Branche auf.
+     * Er stelle eine Map mit folgender Signatur: Map<Branche, List<Kunde>>
+     * <p>
+     * Die List<Kunde> soll alphabetisch nach den Firmennamen sortiert sein.
+     */
+    static Map<Branche, List<Kunde>> task5(List<Berater> beraterList) {
+
+        return null;
+    }
+
+
+    /**
+     * Optionale Aufgabe, falls du immer noch der Meinung bist, dass Stream unleserlicher sind als Java-7-Code:
+     * Wiederhole Aufgabe 5, diesmal aber ohne Streams (╯°□°）╯︵ ┻━┻
+     */
+    static Map<Branche, List<Kunde>> task5b(List<Berater> beraterList) {
+
+        return null;
+    }
+
+
+    /**
+     * Kommst du auch mit Streams in Streams klar?
+     * Erstelle eine Map, die als Key den Beraternamen enthält und als Value alle Branchen, mit denen er zu tun hat.
+     */
+    static Map<String, Set<Branche>> task6(List<Berater> beraterList) {
+
+        return null;
+    }
+
+
+
+    /*
+     * Genug von Collection-Manipulation mit Hilfe von Streams.
+     *
+     * Task 7) Implementiere folgende Methode indem du die Streams für primitive Datentypen nutzt:
+     */
+
+    /**
+     * Diese Methode prüft, ob numberToTest eine Promzahl ist.
+     * <p>
+     * Dazu probiert sie alle Integers (i) von 2 bis numberToTest/2 durch und prüft ob
+     * numberToTest % i == 0 ist. Dies soll parallel auf allen CPUs gesehen und beim erstbesten
+     * Treffer aufhören.
+     *
+     * @param numberToTest wird geprüft ob es eine Primzahl ist
+     * @return true, wenn es sich um eine Primzahl handelt, false, wenn nicht
+     */
+    static boolean checkPrime(int numberToTest) {
+
+        return false;
+    }
+
+
+    /**
+     * Schreibe deinen eigenen Reducer, um die Schuhgrößen aller Berater zu addieren.
+     * <p>
+     * Der Rückgabewert soll genau wie in der Demo ein BigInteger sein. Sonst wäre es ja zu einfach ;-)
+     */
+    static BigInteger task8(List<Berater> beraterList) {
+
+        return null;
+    }
+
 
 }
--- Session 04 - Stream_Optional/src/test/java/AufgabenSession04Test.java
+++ Session 04 - Stream_Optional/src/test/java/AufgabenSession04Test.java
@@ -1,3 +1,241 @@
+import model.Berater;
+import model.Branche;
+import model.Kunde;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class AufgabenSession04Test {
+
+
+    @Test
+    void task2() {
+        //Lösungsvorschlag:
+//        return beraterList.stream()
+//                .filter(berater -> berater.getKunden().size() == 1)
+//                .collect(Collectors.toList());
+
+
+        List<Berater> expectedData = Helper.generateData().stream()
+                .filter(berater -> berater.getKunden().size() == 1)
+                .collect(Collectors.toList());
+
+        List<Berater> actualData = AufgabenSession04.task2(Helper.generateData());
+
+        assertEquals(expectedData, actualData);
+    }
+
+
+    @Test
+    void task3() {
+// Lösungsvorschlag:
+//        return beraterList.stream()
+//                .filter(berater -> berater.getKunden().size() == 1)
+//                .map(Berater::getName)
+//                .collect(Collectors.toSet());
+
+// alternativer Lösungsvorschlag:
+//        beraterList.stream()
+//                .filter(berater -> berater.getKunden().size() == 1)
+//                .distinct()
+//                .map(Berater::getName)
+//                .collect(Collectors.toList());
+
+
+        List<Berater> testData = new ArrayList<>(Helper.generateData());
+        testData.add(new Berater("Dave", 42, Set.of(
+                new Kunde("Lidl", Branche.EDV))));
+        testData.add(new Berater("Dave", 42, Set.of(
+                new Kunde("Lidl", Branche.EDV))));
+
+
+        Set<String> expectedData = testData.stream()
+                .filter(berater -> berater.getKunden().size() == 1)
+                .map(Berater::getName)
+                .collect(Collectors.toSet());
+
+        Collection<String> actualData = AufgabenSession04.task3(testData);
+
+        assertEquals(expectedData.stream().sorted().collect(Collectors.toList()),
+                actualData.stream().sorted().collect(Collectors.toList()));
+    }
+
+
+    @Test
+    void task4() {
+        // Jaja, ich bin gemein: ein Set hat eigentlich keine Reihenfolge.
+        // Aber hat dir vielleicht deine IDE geholfen?
+
+// Lösungsvorschlag:
+//        return beraterList.stream()
+//                .filter(berater -> berater.getKunden().size() >= 2)
+//                .flatMap(berater -> berater.getKunden().stream())
+//                .map(Kunde::getFirmenname)
+//                .sorted()
+//                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        List<Berater> testData = new ArrayList<>(Helper.generateData());
+
+
+        Set<String> expectedData = testData.stream()
+                .filter(berater -> berater.getKunden().size() >= 2)
+                .flatMap(berater -> berater.getKunden().stream())
+                .map(Kunde::getFirmenname)
+                .sorted()
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        Collection<String> actualData = AufgabenSession04.task4(testData);
+
+        assertEquals(expectedData, actualData);
+    }
+
+
+    @Test
+    void task5() {
+// Lösungsvorschlag:
+//        return beraterList.stream()
+//                .flatMap(berater -> berater.getKunden().stream())
+//                .sorted(Comparator.comparing(Kunde::getFirmenname))
+//                .collect(Collectors.groupingBy(Kunde::getBranche));
+
+        List<Berater> testData = new ArrayList<>(Helper.generateData());
+
+        Map<Branche, List<Kunde>> expectedData = testData.stream()
+                .flatMap(berater -> berater.getKunden().stream())
+                .sorted(Comparator.comparing(Kunde::getFirmenname))
+                .collect(Collectors.groupingBy(Kunde::getBranche));
+
+        Map<Branche, List<Kunde>> actualData = AufgabenSession04.task5(testData);
+
+        assertEquals(expectedData, actualData);
+    }
+
+    @Test
+    void task5b() {
+// Lösungsvorschlag:
+//        List<Kunde> toSort = new ArrayList<>();
+//        for (Berater berater : beraterList) {
+//            for (Kunde kunde : berater.getKunden()) {
+//                toSort.add(kunde);
+//            }
+//        }
+//        toSort.sort(Comparator.comparing(Kunde::getFirmenname));
+//        Map<Branche, List<Kunde>> map = new HashMap<>();
+//        for (Kunde kunde : toSort) {
+//            map.computeIfAbsent(kunde.getBranche(), k -> new ArrayList<>()).add(kunde);
+//        }
+//        return map;
+
+        List<Berater> testData = new ArrayList<>(Helper.generateData());
+
+        Map<Branche, List<Kunde>> expectedData = testData.stream()
+                .flatMap(berater -> berater.getKunden().stream())
+                .sorted(Comparator.comparing(Kunde::getFirmenname))
+                .collect(Collectors.groupingBy(Kunde::getBranche));
+
+        Map<Branche, List<Kunde>> actualData = AufgabenSession04.task5b(testData);
+
+        assertEquals(expectedData, actualData);
+    }
+
+
+    @Test
+    void task6() {
+// Lösungsvorschlag:
+//        return beraterList.stream()
+//                .collect(Collectors.toMap(Berater::getName, berater -> berater.getKunden().stream()
+//                        .map(Kunde::getBranche).collect(Collectors.toSet()))
+//                );
+
+        List<Berater> testData = new ArrayList<>(Helper.generateData());
+
+        Map<String, Set<Branche>> expectedData = testData.stream()
+                .collect(Collectors.toMap(Berater::getName, berater -> berater.getKunden().stream()
+                        .map(Kunde::getBranche).collect(Collectors.toSet()))
+                );
+
+        Map<String, Set<Branche>> actualData = AufgabenSession04.task6(testData);
+
+        assertEquals(expectedData, actualData);
+    }
+
+
+    @Test
+    void task7() {
+// Lösungsvorschlag:
+//        boolean noPrime = IntStream.range(2, (numberToTest + 1) / 2)
+//                .parallel()
+//                .anyMatch(i -> numberToTest % i == 0);
+//        return !noPrime;
+
+//        Oder mit:
+//        IntStream.rangeClosed(2, numberToTest / 2)
+
+        // Laufzeit des Tests mit .parallel(): ca. 6 Sek, ohne: ca. 20 Sek
+
+        assertTrue(AufgabenSession04.checkPrime(331));
+        assertTrue(AufgabenSession04.checkPrime(661));
+
+        assertFalse(AufgabenSession04.checkPrime(323));
+        assertFalse(AufgabenSession04.checkPrime(667));
+
+
+        // Versucht bei den hier nicht den Stream Trace zu öffnen :-P
+        assertTrue(AufgabenSession04.checkPrime(1630548889));
+        assertTrue(AufgabenSession04.checkPrime(1819420133));
+        assertTrue(AufgabenSession04.checkPrime(1465144531));
+        assertTrue(AufgabenSession04.checkPrime(1867488269));
+        assertTrue(AufgabenSession04.checkPrime(1393490699));
+        assertTrue(AufgabenSession04.checkPrime(1537557883));
+        assertTrue(AufgabenSession04.checkPrime(1537557883));
+
+        assertFalse(AufgabenSession04.checkPrime(114961163));
+        assertFalse(AufgabenSession04.checkPrime(124272587));
+        assertFalse(AufgabenSession04.checkPrime(479229187));
+        assertFalse(AufgabenSession04.checkPrime(1900872737));
+        assertFalse(AufgabenSession04.checkPrime(1995389579));
+
+
+//        assertEquals(expectedData, actualData);
+    }
+
+
+    @Test
+    void task8() {
+// Lösungsvorschlag:
+//        return beraterList.stream()
+//                .map(Berater::getSchuhgroesse)
+//                .reduce(
+//                        BigInteger.ZERO,
+//                        (BigInteger bigInteger, Integer integer) -> bigInteger.add(BigInteger.valueOf(integer)),
+//                        BigInteger::add
+//                );
+
+        List<Berater> testData = new ArrayList<>(Helper.generateData());
+        testData.add(new Berater("Bigfoot", Integer.MAX_VALUE, Collections.emptySet()));
+
+        BigInteger expectedData = testData.stream()
+                .map(Berater::getSchuhgroesse)
+                .reduce(
+                        BigInteger.ZERO,
+                        (BigInteger bigInteger, Integer integer) -> bigInteger.add(BigInteger.valueOf(integer)),
+                        BigInteger::add
+                );
+        BigInteger actualData = AufgabenSession04.task8(testData);
+
+        assertEquals(expectedData, actualData);
+
+    }
 
 }
